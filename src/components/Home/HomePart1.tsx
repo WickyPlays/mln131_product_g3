9f269df019be0ac87c4fb7817756639cc2617b5a
WickyPlays <nguyenthienbao8601@gmail.com>
2025-01-12 19:20:35 +0700

    UpdateUpdate

--- src/components/Home/HomePart1.tsx
+++ src/components/Home/HomePart1.tsx
@@ -1,11 +1,15 @@
-import './HomePart1.scss'
+import Content1Intro from "./content/Content1Intro";
+import "./HomePart1.scss";
 
 export default function HomePart1() {
   return (
     <div className="home-part1">
-      <div className='content-bg'>
-        
+      <div className="content-bg">
+        <div className="cover"></div>
+        <div className="content">
+          <Content1Intro />
+        </div>
       </div>
     </div>
-  ) 
+  );
 }
---
+++ src/components/Home/content/Content1Intro.tsx
@@ -0,0 +1,26 @@
+import "./Content1Intro.scss";
+
+export default function Content1Intro() {
+  return (
+    <div className="content1-intro">
+      <p className="title">Mục lục</p>
+      <div className="content-list">
+        <p>
+          <span>1</span> Lời nói đầu, giới thiệu chung
+        </p>
+        <p>
+          <span>2</span> Nội dung tư tưởng về đoàn kết quốc tế
+        </p>
+        <p>
+          <span>3</span> Câu hỏi CQ: ""Ở các nước phương Tây hiện tại có rất nhiều quốc gia phát triển, chế độ phúc lợi và chăm sóc sức khỏe cho người dân rất tốt. Việt Nam quá độ lên chủ nghĩa xã hội cần học hỏi gì từ những nước phát triển đó?"
+        </p>
+        <p>
+          <span>4</span> Tư liệu sưu tầmtầm
+        </p>
+        <p>
+          <span>5</span> Minigames!
+        </p>
+      </div>
+    </div>
+  );
+}
