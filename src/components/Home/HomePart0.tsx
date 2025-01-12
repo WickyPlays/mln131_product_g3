ae59837522fc6c1cc9067ba2ead9c9d4d5a57962
WickyPlays <nguyenthienbao8601@gmail.com>
2025-01-12 16:01:06 +0700

    Update color

--- src/components/Home/HomePart0.tsx
+++ src/components/Home/HomePart0.tsx
@@ -14,12 +14,34 @@ export default function HomePart0() {
     <div className="home-part0">
       <div className="bg" style={{ backgroundImage: `url(${bg})` }}></div>
       <div className={`drawer ${isOpen ? "open" : "closed"}`}>
-        <div className={`toggle-button ${isOpen ? "toggle-button-open" : "toggle-button-closed"}`} onClick={toggleDrawer}>
+        <div
+          className={`toggle-button ${
+            isOpen ? "toggle-button-open" : "toggle-button-closed"
+          }`}
+          onClick={toggleDrawer}
+        >
           {isOpen ? "❮" : "❯"}
         </div>
         <h3>MLN131 - Group 3</h3>
         <h1>Chủ nghĩa xã hội và thời kỳ quá độ lên chủ nghĩa xã hội</h1>
         <div className="divider"></div>
+        <div className="credits">
+          <section>
+            <p className="title">Biên soạn</p>
+            <p>SE170112 - Nguyễn Ngọc Tuấn</p>
+            <p>SE171076 - Nguyễn Bình Phương Trâm</p>
+            <p>SE172082 - Từ Thiên Bảo</p>
+            <p>SE170317 - Lê Quốc Khánh</p>
+          </section>
+          <section>
+            <p className="title">Giảng viên hướng dẫn</p>
+            <p>Nguyễn Anh Khoa</p>
+          </section>
+          <section>
+            <p className="title">Mã nguồn</p>
+            <p><a href="https://github.com/WickyPlays/mln131_product_g3">https://github.com/WickyPlays/mln131_product_g3</a></p>
+          </section>
+        </div>
       </div>
     </div>
   );
