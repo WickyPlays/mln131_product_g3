0b6b2933c5bdba0ce4c159a5f9c1acd4b51ee9e7
WickyPlays <nguyenthienbao8601@gmail.com>
2025-01-13 17:54:43 +0700

    Update

--- src/components/Home/content/Content1Intro.tsx
+++ src/components/Home/content/Content1Intro.tsx
@@ -6,19 +6,22 @@ export default function Content1Intro() {
       <p className="title">Mục lục</p>
       <div className="content-list">
         <p>
-          <span>1</span> Lời nói đầu, giới thiệu chung
+          <span>1</span> Chủ nghĩa xã hội
         </p>
         <p>
-          <span>2</span> Nội dung tư tưởng về đoàn kết quốc tế
+          <span>2</span> Thời kì quá độ lên chủ nghĩa xã hội
         </p>
         <p>
           <span>3</span> Câu hỏi CQ: ""Ở các nước phương Tây hiện tại có rất nhiều quốc gia phát triển, chế độ phúc lợi và chăm sóc sức khỏe cho người dân rất tốt. Việt Nam quá độ lên chủ nghĩa xã hội cần học hỏi gì từ những nước phát triển đó?"
         </p>
         <p>
-          <span>4</span> Tư liệu sưu tầmtầm
+          <span>4</span> Tư liệu sưu tầm
         </p>
         <p>
-          <span>5</span> Minigames!
+          <span>5</span> Tương tác với trí tuệ thông minh (AI)
+        </p>
+        <p>
+          <span>5</span> Minigames
         </p>
       </div>
     </div>
