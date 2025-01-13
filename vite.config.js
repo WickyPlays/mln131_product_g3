7ab698f15b72cb621560624f691af61f53a836cb
WickyPlays <nguyenthienbao8601@gmail.com>
2025-01-13 17:34:52 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/mln131_product_g3/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@utils': '/src/components/utils',
-      '@scss': '/src/scss',
-      '@commons': '/src/components/commons',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://simple-gemini-express.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
-
