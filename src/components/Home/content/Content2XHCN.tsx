52da6487780295be3a278a699918197020debb2e
WickyPlays <nguyenthienbao8601@gmail.com>
2025-01-14 17:08:56 +0700

    Update

--- src/components/Home/content/Content2XHCN.tsx
+++ src/components/Home/content/Content2XHCN.tsx
@@ -1,5 +1,12 @@
-import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Button,
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import "./Content2XHCN.scss";
 import { useState } from "react";
 
@@ -63,8 +70,31 @@ export default function Content2XHCN() {
       ),
     },
   ];
+  const section3Choices = [
+    {
+      title: "Kinh tế",
+      content:
+        "Thời kỳ quá độ yêu cầu chuyển đổi từ nền kinh tế lạc hậu, dựa trên sản xuất nhỏ, sang nền kinh tế hiện đại, xã hội hóa cao. Sự phát triển của các thành phần kinh tế, đặc biệt là kinh tế nhà nước và kinh tế tập thể, đóng vai trò chủ đạo. Đồng thời, cần duy trì và cải cách các thành phần kinh tế tư nhân trong khuôn khổ pháp luật để khai thác tiềm năng sản xuất. Việc xây dựng cơ sở hạ tầng, phát triển công nghiệp hóa, hiện đại hóa là nhiệm vụ then chốt để nâng cao năng lực sản xuất và đảm bảo sự ổn định xã hội.",
+    },
+    {
+      title: "Chính trị",
+      content:
+        "Thời kỳ quá độ đòi hỏi thiết lập nền dân chủ xã hội chủ nghĩa, xây dựng và củng cố nhà nước pháp quyền của nhân dân. Sự lãnh đạo của Đảng Cộng sản là yếu tố quyết định để định hướng và điều hành quá trình chuyển đổi. Đồng thời, cần nâng cao vai trò của các tổ chức chính trị - xã hội nhằm tăng cường sự tham gia của quần chúng vào quản lý xã hội. Việc đấu tranh chống các thế lực thù địch và bảo vệ thành quả cách mạng cũng là nhiệm vụ quan trọng trong giai đoạn này.",
+    },
+    {
+      title: "Tư tưởng - văn hóa",
+      content:
+        "Thời kỳ quá độ cần thúc đẩy cuộc cách mạng tư tưởng, xây dựng hệ tư tưởng xã hội chủ nghĩa trở thành hệ tư tưởng chủ đạo. Việc phát triển giáo dục, khoa học, nghệ thuật, và văn hóa là nhằm tạo nên con người mới xã hội chủ nghĩa với tư duy sáng tạo, ý thức cộng đồng, và lòng yêu nước. Đồng thời, phải đấu tranh chống lại các tư tưởng lạc hậu, phản động, bảo vệ nền tảng tư tưởng của Đảng và củng cố niềm tin của nhân dân vào con đường xã hội chủ nghĩa.",
+    },
+    {
+      title: "Xã hội",
+      content:
+        "Thời kỳ quá độ tập trung vào việc giảm bớt bất bình đẳng, nâng cao chất lượng cuộc sống cho nhân dân. Việc cải thiện các chính sách an sinh xã hội, chăm sóc y tế, giáo dục, và đảm bảo việc làm là những ưu tiên hàng đầu. Đồng thời, cần xây dựng một xã hội đoàn kết, hài hòa, trong đó quyền lợi và nghĩa vụ của mọi người được bảo đảm một cách công bằng. Quá trình này đòi hỏi sự phối hợp chặt chẽ giữa các lĩnh vực để hướng tới mục tiêu cuối cùng là xây dựng chủ nghĩa xã hội.",
+    },
+  ];
 
   const [expanded, setExpanded] = useState(false);
+  const [choice3Input, setChoice3Input] = useState(0);
 
   //@ts-ignore
   const handleChange = (panel) => (event, isExpanded) => {
@@ -119,21 +149,133 @@ export default function Content2XHCN() {
         <div className="sect-2">
           <p className="title-label">Các loại thời kỳ quá độ</p>
           <div className="content">
-            Quá độ trực tiếp: Từ chủ nghĩa tư bản phát triển lên chủ nghĩa cộng
-            sản, chưa từng diễn ra. Quá độ gián tiếp: Các nước chưa phát triển
-            qua chủ nghĩa tư bản, như Liên Xô, Trung Quốc, Việt Nam, đang trong
-            thời kỳ này với trình độ khác nhau.
+            <p>
+              <span className="content-tag">Quá độ trực tiếp</span>
+              <span>
+                Là quá trình chuyển từ chủ nghĩa tư bản phát triển lên chủ nghĩa
+                cộng sản một cách trực tiếp, không qua giai đoạn trung gian. Tuy
+                nhiên, trong lịch sử, chưa từng có quốc gia nào thực hiện được
+                điều này. Điều này chủ yếu là do sự cần thiết của việc xây dựng
+                các cơ sở hạ tầng kinh tế, văn hóa, và xã hội để đảm bảo sự phát
+                triển đồng bộ cho chủ nghĩa cộng sản.
+              </span>
+            </p>
+            <p>
+              <span className="content-tag">Quá độ gián tiếp</span>
+              <span>
+                Đây là quá trình chuyển đổi ở các quốc gia chưa trải qua giai
+                đoạn phát triển đầy đủ của chủ nghĩa tư bản, thường phải tiến
+                hành một cách tuần tự qua các giai đoạn cải cách, xây dựng xã
+                hội mới. Các nước như Liên Xô, Trung Quốc, và Việt Nam đã từng
+                bước thực hiện điều này với những trình độ phát triển khác nhau.
+                <ul>
+                  <li>
+                    Ở Liên Xô, quá trình này được thực hiện thông qua các kế
+                    hoạch hóa kinh tế tập trung và quốc hữu hóa các ngành công
+                    nghiệp.
+                  </li>
+                  <li>
+                    Trung Quốc áp dụng các chính sách đổi mới như cải cách kinh
+                    tế và mở cửa, kết hợp yếu tố thị trường với kế hoạch hóa xã
+                    hội chủ nghĩa.
+                  </li>
+                  <li>
+                    Việt Nam hiện tại đang trong thời kỳ xây dựng và phát triển
+                    kinh tế thị trường định hướng xã hội chủ nghĩa, tập trung
+                    vào hội nhập quốc tế, cải thiện chất lượng cuộc sống và duy
+                    trì các giá trị xã hội chủ nghĩa.
+                  </li>
+                </ul>
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="sect-2">
+          <p className="title-label">Con đường rút ngắn</p>
+          <div className="content">
+            <p>
+              Con đường rút ngắn lên chủ nghĩa xã hội không chỉ là một lựa chọn
+              chiến lược mà còn phản ánh khát vọng của nhiều quốc gia muốn thoát
+              khỏi tình trạng lạc hậu và bất bình đẳng. Việc thực hiện con đường
+              này đòi hỏi sự đồng thuận chính trị mạnh mẽ, sự lãnh đạo sáng suốt
+              của chính phủ và sự tham gia tích cực của toàn xã hội.
+            </p>
+            <p>
+              Để thành công, các quốc gia cần xác định rõ các ưu tiên phát
+              triển, đầu tư mạnh mẽ vào giáo dục, y tế và cơ sở hạ tầng công
+              nghệ. Sự chuyển giao công nghệ từ các nước tiên tiến thông qua hợp
+              tác quốc tế và việc xây dựng năng lực nội tại cũng đóng vai trò
+              then chốt. Bên cạnh đó, việc thúc đẩy đổi mới sáng tạo trong các
+              lĩnh vực như trí tuệ nhân tạo, năng lượng tái tạo, và công nghệ
+              sinh học có thể giúp các quốc gia không chỉ theo kịp mà còn vượt
+              lên trong một số lĩnh vực trọng điểm.
+            </p>
+            <p>
+              Tuy nhiên, con đường này không tránh khỏi những thách thức lớn,
+              bao gồm áp lực từ các thế lực bảo thủ, sự cạnh tranh toàn cầu và
+              nguy cơ phụ thuộc vào công nghệ nước ngoài. Do đó, cần xây dựng
+              một mô hình phát triển kinh tế - xã hội độc lập, tự chủ, nhưng vẫn
+              cởi mở và sẵn sàng hội nhập.
+            </p>
           </div>
         </div>
         <div className="sect-3">
-          <p className="title-label">Con đường rút ngắn</p>
-          <div className="content">
-            Các nước lạc hậu, với sự hỗ trợ của giai cấp vô sản quốc tế, có thể
-            tiến thẳng lên chủ nghĩa xã hội, bỏ qua chế độ tư bản chủ nghĩa. Lợi
-            thế toàn cầu hóa và cách mạng công nghiệp 4.0 giúp rút ngắn quá
-            trình phát triển.
+          <p className="title-label">Đặc điểm</p>
+          <div className="sect-content">
+            <p>
+              <ArrowForwardIosIcon className="icon" />
+              <span>
+                Thời kỳ quá độ lên chủ nghĩa xã hội là giai đoạn chuyển đổi cách
+                mạng từ xã hội cũ (tiền tư bản chủ nghĩa hoặc tư bản chủ nghĩa)
+                sang xã hội xã hội chủ nghĩa.
+              </span>
+            </p>
+            <p>
+              <ArrowForwardIosIcon className="icon" />
+              <span>
+                Đây là quá trình phát triển lâu dài và phức tạp, trong đó các
+                yếu tố của xã hội cũ và xã hội mới đan xen, đấu tranh lẫn nhau.
+              </span>
+            </p>
+            <p>
+              <ArrowForwardIosIcon className="icon" />
+              <span>
+                Thời kỳ này đòi hỏi sự thay đổi toàn diện về kinh tế, chính trị,
+                văn hóa, và xã hội dưới sự lãnh đạo của Đảng Cộng sản, nhằm xây
+                dựng nền tảng vật chất, kỹ thuật và tư tưởng cho chủ nghĩa xã
+                hội.
+              </span>
+            </p>
+          </div>
+          <div className="choices-wrapper">
+            <div className="choices">
+              {section3Choices.map((choice, index) => (
+                <Button
+                  className={`choice-btn ${
+                    index == choice3Input && "choice-btn-opened"
+                  }`}
+                  key={index}
+                  onClick={() => setChoice3Input(index)}
+                >
+                  <p className="choice-title">{choice.title}</p>
+                </Button>
+              ))}
+            </div>
+            <div className="choice-text">
+              {section3Choices[choice3Input].content}
+            </div>
           </div>
         </div>
+      </div>
+      <div className="summary">
+        <KeyboardDoubleArrowDownIcon className="summary-icon" />
+        <p className="summary-content">
+          Thời kỳ quá độ lên chủ nghĩa xã hội là giai đoạn tất yếu, chuyển đổi
+          toàn diện về kinh tế, chính trị, văn hóa, xã hội, dưới sự lãnh đạo của
+          Đảng, nhằm xây dựng nền tảng cho chủ nghĩa xã hội, với trọng tâm là
+          phát triển kinh tế hiện đại, nâng cao chất lượng sống và thúc đẩy công
+          bằng, đoàn kết xã hội.
+        </p>
       </div>
     </div>
   );
--- src/components/Home/content/Content3CQ.tsx
+++ src/components/Home/content/Content3CQ.tsx
@@ -1,6 +1,172 @@
 import "./Content3CQ.scss";
+import img1 from "@assets/content-bg.jpg";
+import img2 from "@assets/content-bg.jpg";
+import img3 from "@assets/content-bg.jpg";
+import img4 from "@assets/content-bg.jpg";
+import img5 from "@assets/content-bg.jpg";
+import img6 from "@assets/content-bg.jpg";
+import img7 from "@assets/content-bg.jpg";
+import { useEffect, useRef, useState } from "react";
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 export default function Content3CQ() {
+  const [activeSection, setActiveSection] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const translateX = useRef("translateX(0)");
+  const sectionsTitles = [
+    "Sơ lược",
+    "Về kinh tế",
+    "Về chính trị",
+    "Về phúc lợi xã hội",
+    "Về văn hóa giáo dục",
+    "Về quản trị xã hội",
+    "Về môi trường quốc tế",
+  ];
+
+  const sectionsContent = [
+    <div>
+      <p>
+        Các quốc gia phương Tây nổi tiếng với sự phát triển vượt bậc trong kinh
+        tế, xã hội, và khoa học công nghệ, cùng với hệ thống phúc lợi xã hội
+        tiên tiến, mang lại đời sống ổn định và chất lượng cao cho người dân.
+      </p>
+      <p>
+        Bên cạnh đó, các quốc gia phương Tây còn nổi bật với sự minh bạch và
+        hiệu quả trong quản lý nhà nước, tạo điều kiện cho xã hội vận hành một
+        cách công bằng và bền vững. Các chính sách khuyến khích đổi mới sáng
+        tạo, sự hợp tác chặt chẽ giữa chính phủ, doanh nghiệp, và các tổ chức xã
+        hội cũng là những yếu tố quan trọng thúc đẩy sự phát triển bền vững.
+      </p>
+    </div>,
+    <div>
+      <p>
+        Nền kinh tế đa thành phần: Các nước phương Tây duy trì nền kinh tế thị
+        trường với nhiều thành phần kinh tế hoạt động song song. Việt Nam có thể
+        học cách quản lý và điều tiết các thành phần kinh tế, đặc biệt là phối
+        hợp giữa kinh tế nhà nước và kinh tế tư nhân, nhằm thúc đẩy tăng trưởng
+        kinh tế bền vững.
+      </p>
+      <p>
+        Phát triển lực lượng sản xuất hiện đại: Học hỏi cách áp dụng công nghệ
+        tiên tiến, đầu tư vào khoa học - công nghệ và phát triển lực lượng lao
+        động chất lượng cao để nâng cao năng suất và hiện đại hóa nền kinh tế.
+      </p>
+    </div>,
+    <div>
+      <p>
+        Nhà nước pháp quyền: Học cách xây dựng một nhà nước pháp quyền mạnh mẽ,
+        minh bạch, nơi pháp luật được thượng tôn, nhằm bảo vệ quyền lợi của mọi
+        tầng lớp nhân dân. Dân chủ: Việt Nam có thể tham khảo mô hình dân chủ xã
+        hội của các nước phương Tây, như cách lắng nghe ý kiến của người dân,
+        xây dựng cơ chế giám sát và trách nhiệm giải trình hiệu quả.
+      </p>
+    </div>,
+    <div>
+      <p>
+        Chế độ an sinh xã hội: Học cách thiết lập các chính sách an sinh xã hội
+        bao trùm, như chăm sóc sức khỏe toàn dân, trợ cấp thất nghiệp, và lương
+        hưu. Điều này có thể giúp đảm bảo công bằng xã hội và cải thiện chất
+        lượng sống.
+      </p>
+      <p>
+        Hệ thống y tế: Tham khảo cách các quốc gia phát triển cung cấp dịch vụ y
+        tế miễn phí hoặc chi phí thấp, với cơ sở vật chất hiện đại và nhân lực y
+        tế chất lượng cao.
+      </p>
+    </div>,
+    <div>
+      <p>
+        Tiếp thu tinh hoa văn hóa: Học hỏi từ những giá trị văn hóa tiến bộ, như
+        tư duy đổi mới, đề cao sáng tạo và tôn trọng sự đa dạng văn hóa.
+      </p>
+      <p>
+        Phát triển giáo dục: Cải thiện hệ thống giáo dục theo hướng hiện đại
+        hóa, gắn kết giữa lý thuyết và thực tiễn, tập trung vào nghiên cứu khoa
+        học và đào tạo nhân lực chất lượng cao.
+      </p>
+    </div>,
+    <div>
+      <p>
+        Quản lý hiệu quả: Học từ các nước phương Tây về cách quản trị xã hội
+        hiện đại, ứng dụng công nghệ số trong quản lý hành chính, dịch vụ công,
+        và đảm bảo tính minh bạch. Xây dựng lòng tin: Các nước phát triển chú
+        trọng xây dựng lòng tin giữa nhà nước và người dân thông qua sự minh
+        bạch và các chính sách công bằng. Đây là bài học quan trọng cho Việt Nam
+        trong việc xây dựng chế độ xã hội chủ nghĩa.
+      </p>
+    </div>,
+    <div>
+      <p>
+        Hợp tác quốc tế: Học hỏi cách các quốc gia phương Tây mở rộng hợp tác
+        quốc tế, thúc đẩy hội nhập kinh tế và tận dụng lợi ích của toàn cầu hóa
+        để phát triển đất nước.
+      </p>
+    </div>,
+  ];
+  const images = [null, img2, img3, img4, img5, img6, img7];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll(".section-content-32");
+      const viewportHeight = window.innerHeight;
+
+      let minDistanceToMiddle = Infinity;
+      let active = null;
+
+      sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+        const middleOfViewport = viewportHeight / 2;
+        const distanceToMiddle = Math.abs(
+          rect.top + rect.height / 2 - middleOfViewport
+        );
+
+        if (distanceToMiddle < minDistanceToMiddle) {
+          minDistanceToMiddle = distanceToMiddle;
+          active = index + 1;
+        }
+      });
+
+      setActiveSection(active);
+
+      const totalHeight =
+        document.documentElement.scrollHeight - viewportHeight;
+      const scrollProgress = (window.scrollY / totalHeight) * 100;
+      setProgress(scrollProgress);
+
+      const firstSectionTop = sections[0]?.getBoundingClientRect().top;
+      const lastSectionBottom =
+        sections[sections.length - 1]?.getBoundingClientRect().bottom;
+
+      setShowNavbar(
+        firstSectionTop <= viewportHeight && lastSectionBottom >= 0
+      );
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    const calculateTransform = () => {
+      const navbarContent = document.querySelector(".navbar-content-32");
+      const highlightedItem = document.querySelector(
+        ".nav-item-32.highlighted"
+      );
+
+      if (navbarContent && highlightedItem) {
+        const navbarCenter = navbarContent.offsetWidth / 2;
+        const itemOffset =
+          highlightedItem.offsetLeft + highlightedItem.offsetWidth / 2;
+        return `translateX(${navbarCenter - itemOffset}px)`;
+      }
+      return translateX.current;
+    };
+
+    translateX.current = calculateTransform();
+  }, [activeSection]);
+
   return (
     <div className="content3-cq">
       <div className="cover">
@@ -14,6 +180,110 @@ export default function Content3CQ() {
             độ lên chủ nghĩa xã hội cần học hỏi gì từ những nước phát triển đó?
           </p>
         </div>
+      </div>
+      <div className="content-32">
+        <p className="tag">
+          "Ở các nước phương Tây hiện tại có rất nhiều quốc gia phát triển, chế
+          độ phúc lợi và chăm sóc sức khỏe cho người dân rất tốt. Việt Nam quá
+          độ lên chủ nghĩa xã hội cần học hỏi gì từ những nước phát triển đó?"
+        </p>
+        <div className="content">
+          {sectionsTitles.map((title, index) => (
+            <div
+              key={index}
+              className={`section-content-32 ${
+                activeSection === index + 1 ? "active" : ""
+              }`}
+            >
+              <div
+                className={`content-block ${
+                  index % 2 === 0 || window.innerWidth < 1024 ? "left" : "right"
+                }`}
+              >
+                {window.innerWidth < 1024 || index % 2 === 0 ? (
+                  <>
+                    <div className="text">
+                      <h2 className="section-title">
+                        {index + 1}. {title}
+                      </h2>
+                      <p className="section-content">
+                        {sectionsContent[index]}
+                      </p>
+                    </div>
+                    <div className="image">
+                      {images[index] != null && (
+                        <img src={images[index]} alt={`Section ${index + 1}`} />
+                      )}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="image">
+                      {images[index] != null && (
+                        <img src={images[index]} alt={`Section ${index + 1}`} />
+                      )}
+                    </div>
+                    <div className="text">
+                      <h2 className="section-title">
+                        {index + 1}. {title}
+                      </h2>
+                      <p className="section-content">
+                        {sectionsContent[index]}
+                      </p>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className={`navbar ${showNavbar ? "show" : "hide"}`}>
+          <div
+            className="navbar-content-32"
+            style={{
+              transform: translateX.current,
+              transition: "transform 0.3s ease",
+            }}
+          >
+            {sectionsTitles.map((title, index) => (
+              <span
+                key={index}
+                className={`nav-item-32 ${
+                  activeSection === index + 1 ? "highlighted" : ""
+                }`}
+                onClick={() => {
+                  const sections = document.querySelectorAll(
+                    ".section-content-32"
+                  );
+                  sections[index].scrollIntoView({
+                    behavior: "smooth",
+                    block: "center",
+                  });
+                }}
+              >
+                {title}
+              </span>
+            ))}
+          </div>
+          <div
+            className="progress-bar"
+            style={{
+              width: `${(activeSection * 100) / sectionsTitles.length}%`,
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className="summary">
+        <KeyboardDoubleArrowDownIcon className="summary-icon" />
+        <p className="summary-content">
+          Việt Nam, trong quá trình quá độ lên chủ nghĩa xã hội, cần chọn lọc
+          học hỏi từ các quốc gia phát triển để thúc đẩy sự phát triển kinh tế,
+          cải thiện chất lượng cuộc sống, và xây dựng một xã hội công bằng, văn
+          minh. Tuy nhiên, việc học hỏi cần gắn với điều kiện cụ thể của đất
+          nước, đảm bảo phù hợp với mục tiêu xây dựng chủ nghĩa xã hội và giữ
+          vững bản sắc dân tộc.
+        </p>
       </div>
     </div>
   );
